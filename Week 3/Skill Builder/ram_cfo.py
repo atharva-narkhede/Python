36c84c3cccc2fb70a6f340a5ffa3c59f866f8332
atharva-narkhede <106006803+atharva-narkhede@users.noreply.github.com>
2022-10-04 15:02:12 +0530

    Update ram_cfo.py

--- Week 3/Skill Builder/ram_cfo.py
+++ Week 3/Skill Builder/ram_cfo.py
@@ -1,3 +1,4 @@
+#Ram is the CFO of an MNC. He wants to order the employee salaries in ascending order so that he can do a salary hike based on the salary values of employees. He selects you to do the task of sorting the salaries. Sort the salaries in ascending order and pass on the information to Ram.
 # You are using Python
 a=int(input())
 num=list(map(int, input().split()))
