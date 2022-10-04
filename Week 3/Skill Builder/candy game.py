970603da9c452f611a43acb6452bc7b63b7b43bc
atharva-narkhede <106006803+atharva-narkhede@users.noreply.github.com>
2022-10-04 15:01:21 +0530

    Update candy game.py

--- Week 3/Skill Builder/candy game.py
+++ Week 3/Skill Builder/candy game.py
@@ -1,3 +1,4 @@
+#Candy Game Mona set off with great zeal to the "Fun Fair 2017". There were numerous activities in the fair, though Mona liked the Candy game. Delicious candies were wrapped in colorful foiled sheets with some random numbers on each of the candies.
 n=int(input())
 c=0
 while n>0:
