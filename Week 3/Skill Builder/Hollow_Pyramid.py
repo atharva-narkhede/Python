a08aa3510fa510f86eecd3a1ea8c927213e0dc9d
atharva-narkhede <106006803+atharva-narkhede@users.noreply.github.com>
2022-10-04 15:00:05 +0530

    Update Hollow_Pyramid.py

--- Week 3/Skill Builder/Hollow_Pyramid.py
+++ Week 3/Skill Builder/Hollow_Pyramid.py
@@ -1,3 +1,4 @@
+#Hollow Pyramid The much-awaited event in the entertainment industry every year is the "Screen Awards". This year the event is going to be organized on December 25 to honor the Artists for their professional excellence in Cinema. The Organizers of the event, J&R Events, decided to design some attractive and LED Matrix panel boards for the show promotions all across the venue.
 # You are using Python
 n=int(input())
 sp=n
