044b54f0c7b040da64ae143aa783f066498617a3
atharva-narkhede <106006803+atharva-narkhede@users.noreply.github.com>
2022-10-09 07:16:06 +0530

    Update Connect_the_ropes.py

--- Week 3/Skill Builder/Connect_the_ropes.py
+++ Week 3/Skill Builder/Connect_the_ropes.py
@@ -1,3 +1,6 @@
+#Kids Love Muffins 
+#Louis was celebrating his 10th Birthday and his parents have promised to make the best party ever for him. He will be very happy if he can invite all his friends to this party (he has many friends), but unfortunately, his parents can't invite everyone because they have a limited number of muffins, and they want everyone to be happy. As we all know, kids love to eat a lot of muffins of the same type, let's say a kid will be happy only if he can eat at least K muffins of the same type.
+
 n,k=map(int,input().split())
 L=list(map(int,input().split()))
 rope=L[0]+L[1]-(2*k)
