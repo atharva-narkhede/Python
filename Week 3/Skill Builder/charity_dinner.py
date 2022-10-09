5233adb7a15de6e4d911dec4d9a95c92196d5c4c
atharva-narkhede <106006803+atharva-narkhede@users.noreply.github.com>
2022-10-09 07:20:57 +0530

    Update charity_dinner.py

--- Week 3/Skill Builder/charity_dinner.py
+++ Week 3/Skill Builder/charity_dinner.py
@@ -1,3 +1,6 @@
+#Charity Dinner 
+#WeCanNGO Trust is organizing a charity dinner at St. John’s College. Since older students are both wiser and richer, the members of the trust decide that the price of each ticket will be based on how many years the students have been in the school. A first-year student will buy a PINK ticket, a second-year student will buy a GREEN ticket, a third-year student will buy a RED ticket, and a fourth-year student will buy an ORANGE ticket.Assume that all tickets are sold. Each color of the ticket is uniquely priced. Write a program to output all combinations of tickets that produce exactly the desired amount to be raised. The combinations may appear in a specific order. First ORANGE is considered, then RED, then GREEN, and finally PINK. Also, display the total number of combinations found and the smallest number of tickets to be printed to raise the desired amount so that printing cost is minimized.
+
 p=int(input())
 g=int(input())
 r=int(input())
