8ec39651f39c1008863b7d9723786177f3cc83d6
Atharva Narkhede <106006803+atharva-narkhede@users.noreply.github.com>
2022-10-14 09:40:44 +0530

    Update count_number_of_strings.py

--- Week 4/Challenge Yourself/count_number_of_strings.py
+++ Week 4/Challenge Yourself/count_number_of_strings.py
@@ -1,9 +1,12 @@
 #To count the number of strings where the string length is 2 or more and the first and last characters are the same from a given list of strings. Sample List : ['abc', 'xyz', 'aba', '1221'] Expected Result : 2
 
-n = str(input())
-count = 0
-string_list = input().split()
-for string in string_list:
-    if len(string) >= 2 and string[0] == string[len(string)-1]:
-        count+=1
-print(count)
+def match_words(words):
+  ctr = 0
+
+  for word in words:
+    if len(word) > 2 and word[0] == word[-1]:
+      ctr += 1
+  return ctr
+n=int(input())
+words = input().split()
+print(match_words(words))
