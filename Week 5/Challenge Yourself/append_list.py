d6723cbcea1617d1ebb5e129a7d7be396b95ea4d
Adi Vishnu Avula <95145136+adivishnu-a@users.noreply.github.com>
2022-11-10 11:19:47 +0530

    Create append_list.py
    Write a program to append one list to another list.

---
+++ Week 5/Challenge Yourself/append_list.py
@@ -0,0 +1,13 @@
+a=int(input())
+x=[]
+for i in range(1):
+    c=str(input()).split()
+    for j in c:
+        x.append(int(j))
+b=int(input())
+y=[]
+for i in range(1):
+    c=str(input()).split()
+    for j in c:
+        x.append(int(j))
+print(x)
