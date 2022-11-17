7525ae76bbe673c8bcdd811406a7a933cbe9ffc6
Atharva Narkhede <106006803+atharva-narkhede@users.noreply.github.com>
2022-11-17 15:29:58 +0530

    Update grammatical.py

--- Week 6/Skill Builder/grammatical.py
+++ Week 6/Skill Builder/grammatical.py
@@ -1,13 +1,9 @@
 #A string S is passed as the input. The program must print the number of articles in S.
 #The string S passed as the input need not be correct grammatically.
-import string
-input_string = input().strip()
-count = 0
-for char in string.punctuation:
-    if char in input_string:
-        input_string = input_string.replace(char," ")
-words = [word for word in input_string.split()]    
-for word in words:
-    if word == 'an' or word == 'a' or word == 'the':
-        count = count + 1
-print(count)
+
+s=input()
+s=s.lower()
+s=s.split()
+for i in range(0,len(s)):
+    s[i]="".join(e for e in s[i] if e.isalnum())
+print(s.count("a")+s.count("an")+s.count("the"))
