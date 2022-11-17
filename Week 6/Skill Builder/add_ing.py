3b2ddca472ce31b0b3ec2a4708a36ac21ca9de6f
Atharva Narkhede <106006803+atharva-narkhede@users.noreply.github.com>
2022-11-17 07:58:43 +0530

    Update add_ing.py

--- Week 6/Skill Builder/add_ing.py
+++ Week 6/Skill Builder/add_ing.py
@@ -1,4 +1,7 @@
-#Write a program to add 'ing' at the end of a given string (length should be at least 3).
+#Write a program to add 'ing' at the end of a given string (length should be at least 3). 
+#If the given string already ends with 'ing' then add 'ly' instead. 
+#If the string length of the given string is less than 3, leave it unchanged.
+
 string = input()
 if string.endswith('ing'):
     print(string + "ly")
