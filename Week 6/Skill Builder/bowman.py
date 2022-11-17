50b54afabd549e011f7c09417a8aa719ed30cbfd
Atharva Narkhede <106006803+atharva-narkhede@users.noreply.github.com>
2022-11-17 07:59:43 +0530

    Update bowman.py

--- Week 6/Skill Builder/bowman.py
+++ Week 6/Skill Builder/bowman.py
@@ -1,4 +1,8 @@
-#Bowman planned a vacation to Alphabet Land.
+#Bowman planned a vacation to Alphabet Land. He had to solve a problem to enter Alphabet Land. 
+#He was given N recipes made up of lower case English alphabets. The recipes were indexed from 1 to N. R[1], R[2], ….., R[N]. 
+#He had to group identical recipes and put them into bags. Help Bowman print the indices of the recipes in each bag.
+
+
 n = int(input())
 recipies_list = []
 for i in range(n):
