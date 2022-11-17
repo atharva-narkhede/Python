1a26c70bfc4ce536c4687333c9205ed91356667a
Atharva Narkhede <106006803+atharva-narkhede@users.noreply.github.com>
2022-11-17 15:32:25 +0530

    Update minimum_dist_w1_w2.py

--- Week 6/Skill Builder/minimum_dist_w1_w2.py
+++ Week 6/Skill Builder/minimum_dist_w1_w2.py
@@ -1,22 +1,13 @@
 #A string S is passed as the input. Two words W1 and W2, which are present in the string S are also passed as the input. The program must find the minimum distance D between W1 and W2 in S (in forward or reverse order) and print D as the output.
 
 
-def distance(sentence,w1,w2):
-    if w1 == w2:
-        return 0
-    words = sentence.split()
-    min_dist = len(words) + 1
-    for index in range(len(words)):
-        if(words[index] == w1):
-            for search in range(len(words)):
-                if(words[search] == w2):
-                    curr = abs(index - search)
-                    if curr < min_dist:
-                        min_dist = curr
-                
-    return min_dist
-
-sentence = input().strip()
-word1 = input().strip()
-word2 = input().strip()
-print(distance(sentence,word1,word2))
+s=input().split()
+w1=input().strip()
+w2=input().strip()
+mi=len(s)
+for i in range(0,len(s)):
+    if s[i]==w1:
+        for j in range(0,len(s)):
+            if s[j]==w2 and mi>abs(i-j) and i!=j:
+                mi=abs(i-j)
+print(mi)
