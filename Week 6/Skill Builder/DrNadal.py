c5c239d1619cb361b93725e7e4da19fceb64928d
Atharva Narkhede <106006803+atharva-narkhede@users.noreply.github.com>
2022-11-17 07:54:41 +0530

    Update DrNadal.py

--- Week 6/Skill Builder/DrNadal.py
+++ Week 6/Skill Builder/DrNadal.py
@@ -1,4 +1,5 @@
-#Dr. Nadal is given a string of ‘n’ characters(S[1], S[2],...., S[n]).
+#Dr. Nadal is given a string of ‘n’ characters(S[1], S[2],...., S[n]). Then he is given Q ranges of the form [L R]. Determine of S[L], S[L+1]... S[R] is a palindrome.
+
 def check_palindrome(string,start,end):
     check_string = string[start:end]
     if(check_string[::] == check_string[::-1]):
