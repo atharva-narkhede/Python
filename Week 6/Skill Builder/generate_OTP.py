9bdc183f682205a1c015094b6f66a245404095e7
Atharva Narkhede <106006803+atharva-narkhede@users.noreply.github.com>
2022-11-17 07:55:47 +0530

    Update and rename generateOTP.py to generate_OTP.py

--- Week 6/Skill Builder/generate_OTP.py
+++ Week 6/Skill Builder/generate_OTP.py
@@ -1,4 +1,9 @@
-#Generate OTP
+#Write a program to obtain an integer from the user and generate the OTP using the below steps.
+#1.separate digits which are in odd place index (index starts from 0)
+#2.square those numbers
+#3. Return the first 4 digits.
+
+
 input_integer = input()
 seperate_odd = []
 for i in range(len(input_integer)):
