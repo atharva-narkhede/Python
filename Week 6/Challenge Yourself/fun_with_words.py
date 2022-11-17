75e29912cf0420db76e472221d766dce7f28bc06
Atharva Narkhede <106006803+atharva-narkhede@users.noreply.github.com>
2022-11-17 08:07:09 +0530

    Update and rename juniors_network_cartoon.py to fun_with_words.py

--- Week 6/Challenge Yourself/fun_with_words.py
+++ Week 6/Challenge Yourself/fun_with_words.py
@@ -1,4 +1,9 @@
-# "Juniors Network" Cartoon Channel is the favorite channel of all the kids in the city. 
+#"Juniors Network" Cartoon Channel is the favorite channel of all the kids in the city. 
+#As it is vacation time, the channel had introduced several new segments to engage the kids in a more creative manner. 
+#"Fun with Words" is one such activity involving English alphabets, where school kids were invited to participate.
+
+
+
 n = [int(i) for i in input().split()][:2]
 st = [str(i) for i in input().split()][:n[0]]
 arr = [None]*n[1]
