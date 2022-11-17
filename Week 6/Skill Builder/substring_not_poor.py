534a8b1d21af400d3115eb53a1253312f518a6cc
Atharva Narkhede <106006803+atharva-narkhede@users.noreply.github.com>
2022-11-17 08:00:50 +0530

    Update and rename substring.py to substring_not_poor.py

--- Week 6/Skill Builder/substring_not_poor.py
+++ Week 6/Skill Builder/substring_not_poor.py
@@ -1,4 +1,6 @@
-#Write a Python program to find the first appearance of the substring
+#Write a Python program to find the first appearance of the substring 'not' and 'poor' from a given string, if 'not' follows the 'poor', replace the whole 'not'...'poor' substring with 'good'. Return the resulting string
+
+
 def get_result(string):
     if string.find("not") != -1:
         start = string.find("not")
