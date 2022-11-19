9715c14dc72b7dc3b5bea86b190094afe22fb68c
Atharva Narkhede <106006803+atharva-narkhede@users.noreply.github.com>
2022-11-19 19:48:59 +0530

    Create pow_class.py

---
+++ Week 10/Skill Builder/pow_class.py
@@ -0,0 +1,23 @@
+#Create a class called Pow. Write a Python program to implement pow(x, n).
+
+class py_solution:
+   def pow(self, x, n):
+        if x==0 or x==1 or n==1:
+            return x 
+
+        if x==-1:
+            if n%2 ==0:
+                return 1
+            else:
+                return -1
+        if n==0:
+            return 1
+        if n<0:
+            return 1/self.pow(x,-n)
+        val = self.pow(x,n//2)
+        if n%2 ==0:
+            return val*val
+        return val*val*x
+x=int(input())
+n=int(input())
+print(py_solution().pow(x, n));
