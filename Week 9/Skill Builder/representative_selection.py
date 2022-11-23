a540e2a655d91cca2236b794ebd1b3003b94c43c
Atharva Narkhede <106006803+atharva-narkhede@users.noreply.github.com>
2022-11-23 10:00:25 +0530

    Create representative_selection.py

---
+++ Week 9/Skill Builder/representative_selection.py
@@ -0,0 +1,7 @@
+#You are using a program to assist in the representative selection in your class. 
+#Three persons are interested in contesting for the post. Each person is given a nomination id like ‘10’,’20’,’30’. 
+#Every student in your class enters only the nomination id in the system.
+
+
+
+    
