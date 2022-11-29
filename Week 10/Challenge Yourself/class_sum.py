258db88eaaf243361bc4423f75b3146086c995ce
Atharva Narkhede <106006803+atharva-narkhede@users.noreply.github.com>
2022-11-29 07:43:35 +0530

    Create class_sum.py

---
+++ Week 10/Challenge Yourself/class_sum.py
@@ -0,0 +1,26 @@
+#Create a class called Sum. Write a Python program to find the three elements that sum to zero from a set (array) of n real numbers.
+
+
+class Sum:
+ def threeSum(self, nums):
+        nums, result, i = sorted(nums), [], 0
+        while i < len(nums) - 2:
+            j, k = i + 1, len(nums) - 1
+            while j < k:
+                if nums[i] + nums[j] + nums[k] < 0:
+                    j += 1
+                elif nums[i] + nums[j] + nums[k] > 0:
+                    k -= 1
+                else:
+                    result.append([nums[i], nums[j], nums[k]])
+                    j, k = j + 1, k - 1
+                    while j < k and nums[j] == nums[j - 1]:
+                        j += 1
+                    while j < k and nums[k] == nums[k + 1]:
+                        k -= 1
+            i += 1
+            while i < len(nums) - 2 and nums[i] == nums[i - 1]:
+                i += 1
+        return result
+n=list(map(int,input().split()))
+print(Sum().threeSum(n))
